7951edee50007b458be68b5c4dd47e4a0419bc72
Pankaj Kumar Sahu <pankajsahuhero@gmail.com>
2022-10-01 22:28:19 +0530

    Update god.cpp

--- god.cpp
+++ god.cpp
@@ -12,3 +12,18 @@ public:
     
     }
 };
+
+class Solution1 {
+public:
+    long long reverse(int x){
+    
+    long long num=0;
+    while(x){
+        int a=x%10;
+        num=(num*10)+a;
+        x=x/10;
+    }
+    return (num<INT_MIN || num>INT_MAX) ? 0 : num;
+    
+    }
+};
